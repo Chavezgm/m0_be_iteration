fb871bcb9fe5089e5e8a3880cc36bf2dfbba48c0
Martin Chavez <martinchavez1203@gmail.com>
2023-08-01 20:15:41 -0600

    chal 2 complete

--- each.rb
+++ each.rb
@@ -12,6 +12,11 @@ end
 # Challenge 2
 # Use the each method to print out the words that are exactly 3 characters in length
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+words.each do |word|
+  if word.length == 3
+    puts "The word is #{word}!"
+  end
+end
 
 
 
